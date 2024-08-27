8211499f64f76f94d541814d8ab66c64fad9eab6
Foysal1999 <foysalrahman838@gmail.com>
2024-08-27 20:25:17 +0600

    first commit

---
+++ foodorder.sql
@@ -0,0 +1,268 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 22, 2019 at 02:53 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `foodorder`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `Name` varchar(250) NOT NULL,
+  `Email` varchar(250) NOT NULL,
+  `Mobile` varchar(250) NOT NULL,
+  `Subject` varchar(250) NOT NULL,
+  `Message` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`Name`, `Email`, `Mobile`, `Subject`, `Message`) VALUES
+('Shad', 'shad@gmail.com', '01855769160', 'Nice Food', 'Food of this resturent is great');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `username` varchar(30) NOT NULL,
+  `fullname` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`username`, `fullname`, `email`, `contact`, `address`, `password`) VALUES
+('Foysal', 'Md. Foysal Rahman', 'foysal@gmail.com', '01764992209', 'Munnafer mor, Boalia, Rajshahi', 'foysal'),
+('Emon', 'Abdullad Al Emon', 'emon@gmail.com', '01724066715', 'Munnafer mor, Boalia, Rajshahi', 'emon');
+COMMIT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food`
+--
+
+CREATE TABLE `food` (
+  `F_ID` int(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `price` int(30) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `R_ID` int(30) NOT NULL,
+  `images_path` varchar(200) NOT NULL,
+  `options` varchar(10) NOT NULL DEFAULT 'ENABLE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`F_ID`, `name`, `price`, `description`, `R_ID`, `images_path`, `options`) VALUES
+(58, 'Juicy Masala Paneer Kathi Roll', 40, 'Juicy Masala Paneer Kathi Roll loaded with Masala Paneer chunks, onion & Mayo.', 1, 'images/Masala_Paneer_Kathi_Roll.jpg', 'ENABLE'),
+(59, 'Meurig Fish', 60, 'Try Meurig - A whole Pomfret fish grilled with tangy marination & served with grilled onions and tomatoes.', 1, 'images/Meurig.jpg', 'ENABLE'),
+(60, 'Chocolate Hazelnut Truffle', 99, 'Lose all senses over this very delicious chocolate hazelnut truffle.', 1, 'images/Chocolate_Hazelnut_Truffle.jpg', 'ENABLE'),
+(61, 'Happy Happy Choco Chip Shake', 80, 'Happy Happy Choco Chip Shake - a perfect party sweet treat.', 1, 'images/Happy_Happy_Choco_Chip_Shake.jpg', 'ENABLE'),
+(62, 'Spring Rolls', 65, 'Delicious Spring Rolls by Dragon Hut, Delhi. Order now!!!', 1, 'images/Spring_Rolls.jpg', 'ENABLE'),
+(63, 'Baahubali Thali', 75, 'Baahubali Thali is accompanied by Kattapa Biriyani, Devasena Paratha, Bhalladeva Patiala Lassi', 1, 'images/Baahubali_Thali.jpg', 'ENABLE'),
+(65, 'Coffee', 25, 'concentrated coffee made by forcing pressurized water through finely ground coffee beans.', 1, 'images/coffee.jpg', 'DISABLE'),
+(66, 'Tea', 20, 'The simple elixir of tea is of our natural world.', 1, 'images/tea.jpg', 'DISABLE'),
+(68, 'Paneer', 85, 'it', 1, 'images/paneer pakora.jpg', 'DISABLE'),
+(69, 'Coffee', 25, 'concentrated coffee made by forcing pressurized water through finely ground coffee beans.', 1, 'images/coffee.jpg', 'ENABLE'),
+(70, 'Tea', 20, 'The simple elixir of tea is of our natural world.', 1, 'images/tea.jpg', 'ENABLE'),
+(71, 'Samosa', 40, 'Cocktail Crispy Samosa..', 1, 'images/samosa.jpg', 'ENABLE'),
+(72, 'Paneer Pakora', 45, 'it gives whole new dimension even to the most boring or dull vegetable', 1, 'images/paneer pakora.jpg', 'ENABLE'),
+(73, 'Puff', 35, 'Vegetable Puff, a snack with crisp-n-flaky outer layer and mixed vegetables stuffing', 1, 'images/puff.jpg', 'ENABLE'),
+(74, 'Pizza', 200, 'Good and Tasty ', 1, 'Pizza.jpg', 'DISABLE'),
+(75, 'French Fries', 60, 'Pure Veg and Tasty.', 1, 'frenchfries.jpg', 'DISABLE'),
+(76, 'Pakora', 35, 'Pure Vegetable and Tasty.', 1, 'images/Pakora.jpg', 'DISABLE'),
+(77, 'Pizza', 200, 'Pure Vegetable and Tasty.', 1, 'images/Pizza.jpg', 'ENABLE'),
+(78, 'French Fries', 75, 'Pure Veg and Tasty.', 1, 'images/frenchfries.jpg', 'ENABLE'),
+(79, 'Pakora', 45, 'TASTY', 1, 'images/Pakora.jpg', 'ENABLE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manager`
+--
+
+CREATE TABLE `manager` (
+  `username` varchar(30) NOT NULL,
+  `fullname` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `manager`
+--
+
+INSERT INTO `manager` (`username`, `fullname`, `email`, `contact`, `address`, `password`) VALUES
+('Foysal', 'Foysal Rahman', 'foysal@gmail.com', '01764992209', 'Munnafer mor, Boalia, Rajshahi', '1234'),
+('Emon', 'Abdullah Al Emon', 'emon@gmail.com', '01724066715', 'Munnafer mor, Boalia, Rajshahi', '1234');
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_ID` int(30) NOT NULL,
+  `F_ID` int(30) NOT NULL,
+  `foodname` varchar(30) NOT NULL,
+  `price` int(30) NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `order_date` date NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `R_ID` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `orders`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurants`
+--
+
+CREATE TABLE `restaurants` (
+  `R_ID` int(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `M_ID` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `restaurants`
+--
+
+INSERT INTO `restaurants` (`R_ID`, `name`, `email`, `contact`, `address`, `M_ID`) VALUES
+(1, 'Dokan Nai Cafe', 'dokannai@restaurant.com', '01317318422', 'Munnafer mor,Boalia Rajshahi', 'Foysal'),
+(1, 'Dokan Nai Cafe', 'dokannai@restaurant.com', '01724088715', 'Munnafer mor,Boalia Rajshahi', 'Emon');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `food`
+--
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`F_ID`,`R_ID`),
+  ADD KEY `R_ID` (`R_ID`);
+
+--
+-- Indexes for table `manager`
+--
+ALTER TABLE `manager`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_ID`),
+  ADD KEY `F_ID` (`F_ID`),
+  ADD KEY `username` (`username`),
+  ADD KEY `R_ID` (`R_ID`);
+
+--
+-- Indexes for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  ADD PRIMARY KEY (`R_ID`),
+  ADD UNIQUE KEY `M_ID_2` (`M_ID`),
+  ADD KEY `M_ID` (`M_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `food`
+--
+ALTER TABLE `food`
+  MODIFY `F_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+
+--
+-- AUTO_INCREMENT for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  MODIFY `R_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `food`
+--
+ALTER TABLE `food`
+  ADD CONSTRAINT `food_ibfk_1` FOREIGN KEY (`R_ID`) REFERENCES `restaurants` (`R_ID`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`F_ID`) REFERENCES `food` (`F_ID`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`username`) REFERENCES `customer` (`username`),
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`R_ID`) REFERENCES `restaurants` (`R_ID`);
+
+--
+-- Constraints for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  ADD CONSTRAINT `restaurants_ibfk_1` FOREIGN KEY (`M_ID`) REFERENCES `manager` (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ onlinepay.php
@@ -0,0 +1,1 @@
+<h1> Online Payment Is Under Process</h1>
